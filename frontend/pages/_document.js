67f044798c6076b2b8509ed74165cfa74acd2324
shovancak <s.hovancak@gmail.com>
2021-04-12 22:24:31 +0200

    Add styles and styled components.

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,9 +1,20 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styledTags = sheet.getStyleElement();
+    return { ...page, styledTags };
+  }
+
   render() {
     return (
       <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
