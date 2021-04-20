ecc64bfbe94dfe471b64536c048581bae53b5967
shovancak <s.hovancak@gmail.com>
2021-04-20 18:50:11 +0200

    Add eslint disable comments.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
     const page = renderPage((App) => (props) =>
+      // eslint-disable-next-line react/jsx-props-no-spreading
       sheet.collectStyles(<App {...props} />)
     );
     const styledTags = sheet.getStyleElement();
